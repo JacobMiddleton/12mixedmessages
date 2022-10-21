bdde6d2930fdc6549402256f8cf55de6932e7ee5
Jacob Middleton <109218193+JacobMiddleton@users.noreply.github.com>
2022-10-21 07:46:11 -0400

    add array of nouns

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-// array of thirty present-tense verbs
+// array of present-tense verbs
 
-const verb = [
+const verbs = [
     walking, running, jumping, surfing, laughing, hugging, typing, lifting, yelling, butchering,
      climbing, standing, dancing, eating, blowing, cutting, scraping, banging, chopping, driving,
      sneezing, reading, praying, twisting, fighting, digging, sliding, pouring, biking, texting
+];
+
+// array of singular nouns
+
+const nouns = [
+    cat, harmonica, house, fireplace, freind, appendix, Ferrari, pillow, condo, squirrel,
+     computer, balloon, mouse, chair, trailer, moose, lion, chipmunk, hotel, pancake, 
+     arrow, guitar, calculator, rifle, tree, monkey, brother, sister, girl, boy 
 ];
