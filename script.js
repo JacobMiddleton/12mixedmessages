fad94b29ad1d7d69e8f8b4d9f635415a434ba121
Jacob Middleton <109218193+JacobMiddleton@users.noreply.github.com>
2022-10-21 08:10:53 -0400

    add quotes to array items & format document

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 // array of present-tense verbs
 
 const verbs = [
-    walking, running, jumping, surfing, laughing, hugging, typing, lifting, yelling, butchering,
-     climbing, standing, dancing, eating, blowing, cutting, scraping, banging, chopping, driving,
-     sneezing, reading, praying, twisting, fighting, digging, sliding, pouring, biking, texting
+    'walking', 'running', 'jumping', 'surfing', 'laughing', 'hugging', 'typing', 'lifting', 'yelling', 'butchering',
+    'climbing', 'standing', 'dancing', 'eating', 'blowing', 'cutting', 'scraping', 'banging', 'chopping', 'driving',
+    'sneezing', 'reading', 'praying', 'twisting', 'fighting', 'digging', 'sliding', 'pouring', 'biking', 'texting'
 ];
 
 // array of singular nouns
 
 const nouns = [
-    cat, harmonica, house, fireplace, freind, appendix, Ferrari, pillow, condo, squirrel,
-     computer, balloon, mouse, chair, trailer, moose, lion, chipmunk, hotel, pancake, 
-     arrow, guitar, calculator, rifle, tree, monkey, brother, sister, girl, boy 
+    'cat', 'harmonica', 'house', 'fireplace', 'freind', 'appendix', 'Ferrari', 'pillow', 'condo', 'squirrel',
+    'computer', 'balloon', 'mouse', 'chair', 'trailer', 'moose', 'lion', 'chipmunk', 'hotel', 'pancake',
+    'arrow', 'guitar', 'calculator', 'rifle', 'tree', 'monkey', 'brother', 'sister', 'girl', 'boy'
 ];
 
 // array of adjectives 
 
 const adjectives = [
-    yellow, beutiful, grumpy, sad, round, werid, flat, powerful, green, smelly,
-    joyful, courteous, dependant, pale, ritzy, glowing, sparkling, blue, brilliant, old,
-    happy, fat, hard, enormous, dreary, upbeat, graceful, wealthy, pink, fast
+    'yellow', 'beutiful', 'grumpy', 'sad', 'round', 'werid', 'flat', 'powerful', 'green', 'smelly',
+    'joyful', 'courteous', 'dependant', 'pale', 'ritzy', 'glowing', 'sparkling', 'blue', 'brilliant', 'old',
+    'happy', 'fat', 'hard', 'enormous', 'dreary', 'upbeat', 'graceful', 'wealthy', 'pink', 'fast'
 ];
