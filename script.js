3ac1b7fc77b809b1575512c693208ea12bd57241
Jacob Middleton <109218193+JacobMiddleton@users.noreply.github.com>
2022-10-21 07:25:14 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// array of thirty present-tense verbs
+
+const verb = [
+    walking, running, jumping, surfing, laughing, hugging, typing, lifting, yelling, butchering,
+     climbing, standing, dancing, eating, blowing, cutting, scraping, banging, chopping, driving,
+     sneezing, reading, praying, twisting, fighting, digging, sliding, pouring, biking, texting
+];
